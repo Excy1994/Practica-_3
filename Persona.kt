a2d3e0028ca0d9548d1b6339f1e886ca9f78a9be
Excy1994 <obregondoribel@gmail.com>
2020-11-29 13:48:39 -0600

    persona cambio dos.kt

--- Persona.kt
+++ Persona.kt
@@ -1,20 +1,24 @@
-class Persona{
-    var nombre:String = ""
-    var apellido:String = ""
-    constructor(){
-    }
-    constructor(n:String, a:String):this()
-    {
-    nombre=n
-    apellido=a
-    }
-   }
-   fun main(){
-    var persona = Persona("Yesser","Miranda")
-    var nueva = Persona()
-    nueva.nombre = "Juan"
-    nueva.apellido = "Lopez"
-    println(persona.nombre)
-    println(nueva.nombre)
+// class Persona{
+//     var nombre:String = ""
+//     var apellido:String = ""
+//     constructor(){
 
-   }
+//     }
+//     constructor(n:String, a:String):this()
+//     {
+//         nombre=n
+//         apellido=a
+//     }
+// }
+
+class Persona(var nombre:String, var apellido:String)
+
+fun main(){
+    var persona = Persona("Yesser","Miranda")
+    // var nueva = Persona()
+    // nueva.nombre = Persona("Juan")
+    // nueva.apellido = "Lopez"
+
+    println(persona.nombre)
+    //println(nueva.nombre)
+}
